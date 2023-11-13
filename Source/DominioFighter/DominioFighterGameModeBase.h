d529fd7630e110dcca656c764217d3b13345d93a
Panchoneit0r <perromendigo321@gmail.com>
2023-11-12 20:32:37 -0600

    first commit

---
+++ Source/DominioFighter/DominioFighterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DominioFighterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DOMINIOFIGHTER_API ADominioFighterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
